0b868cda296f97c8e0f62f005b079134680a447c
angonomholab-cpu <angonomholab@gmail.com>
2026-08-08 16:12:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2347,9 +2347,9 @@ function localGenerateNTPHtml(patientsArray) {
                 width: 100% !important; 
                 max-width: 100% !important;
                 height: auto !important; /* 🟢 Binago from 96vh to auto para humaba kung kailangan */
-                min-height: 100% !important; 
+                min-height: 50% !important; 
                 margin: 0 !important; 
-                padding: 6mm 10mm !important; 
+                padding: 2mm 5mm !important; 
                 border: none !important; 
                 box-shadow: none !important; 
                 overflow: visible !important; /* 🟢 Dito lilitaw ang mga footnotes na naputol! */
