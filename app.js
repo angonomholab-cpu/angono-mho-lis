1b458fd46d1eaf2e5cd7cc5534901cdd51152b65
angonomholab-cpu <angonomholab@gmail.com>
2026-08-08 16:24:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2352,6 +2352,7 @@ function localGenerateNTPHtml(patientsArray) {
                 padding: 3mm 6mm !important; 
                 border: none !important; 
                 box-shadow: none !important; 
+                zoom: 0.64;
                 overflow: visible !important; /* 🟢 Dito lilitaw ang mga footnotes na naputol! */
                 page-break-after: always;
                 page-break-inside: avoid;
